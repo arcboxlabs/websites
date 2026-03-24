dd15b65f0b7f2ae3b47a17fa03240fa368f63aee
SukkaW <isukkaw@gmail.com>
2026-03-25 00:06:52 +0800

    fix(blog): custom mdx components accept custom className

--- src/app/(landing)/blog/[slug]/components/mdx/heading.tsx
+++ src/app/(landing)/blog/[slug]/components/mdx/heading.tsx
@@ -1,16 +1,17 @@
 import { Slot } from '@radix-ui/react-slot';
+import { cn } from '@/lib/utils';
 
 interface HeadingProps extends React.ComponentProps<'h1'> {
   asChild?: boolean
 }
 
-export function Heading({ asChild = false, id, children, ...props }: HeadingProps) {
+export function Heading({ asChild = false, id, children, className, ...props }: HeadingProps) {
   const Comp = asChild ? Slot : 'h1';
 
   return (
     <Comp
       id={id}
-      className="font-medium tracking-tight leading-[140%]"
+      className={cn('font-medium tracking-tight leading-[140%]', className)}
       {...props}
     >
       {id
