3b534a982736cc337945bb934a63d6d590ed9942
SukkaW <isukkaw@gmail.com>
2026-03-27 11:50:04 +0800

    feat(blog): heading link icon

--- src/app/(landing)/blog/[slug]/components/mdx/heading.tsx
+++ src/app/(landing)/blog/[slug]/components/mdx/heading.tsx
@@ -1,26 +1,30 @@
-import { Slot } from '@radix-ui/react-slot';
+import { Root as SlotRoot, Slottable } from '@radix-ui/react-slot';
 import { cn } from '@/lib/utils';
+import { LinkIcon } from 'lucide-react';
 
 interface HeadingProps extends React.ComponentProps<'h1'> {
   asChild?: boolean
 }
 
 export function Heading({ asChild = false, id, children, className, ...props }: HeadingProps) {
-  const Comp = asChild ? Slot : 'h1';
+  const Comp = asChild ? SlotRoot : 'h1';
 
   return (
     <Comp
       id={id}
-      className={cn('font-medium tracking-tight leading-[140%]', className)}
+      className={cn('font-medium tracking-tight leading-[140%]', asChild ? '' : className)}
       {...props}
     >
-      {id
-        ? (
-          <a href={`#${id}`} className="no-underline hover:underline">
-            {children}
-          </a>
-        )
-        : children}
+      {
+        id
+          ? (
+            <a href={`#${id}`} className={cn('no-underline hover:underline flex flex-row items-center gap-2', asChild ? className : '')}>
+              <Slottable>{children}</Slottable>
+              <LinkIcon className="size-3.5 shrink-0 opacity-0 transition-opacity peer-hover:opacity-100" />
+            </a>
+          )
+          : <Slottable>{children}</Slottable>
+      }
     </Comp>
   );
 }
