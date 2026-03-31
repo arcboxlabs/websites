cacb5f6d7741c5e4109b3e5a6cc15fe03ad08293
SukkaW <isukkaw@gmail.com>
2026-03-31 17:24:24 +0800

    fix(blog): thumbnail handling fix

--- src/app/(landing)/blog/components/blog-thumbnail.tsx
+++ src/app/(landing)/blog/components/blog-thumbnail.tsx
@@ -1,6 +1,6 @@
 import 'server-only';
 
-import path from 'node:path';
+import path from 'node:path/posix';
 import { hasProtocol } from 'ufo';
 
 import Image from 'next/image';
@@ -20,7 +20,7 @@ export default async function BlogThumbnail({ src, alt, placeholder, ...restProp
       process.emitWarning(`[BlogThumbnail] blur placeholder is not supported for remote images, ignoring: ${src}`);
     }
 
-    return <Image src={src} alt={alt} {...restProps} />;
+    return <Image src={src} alt={alt} placeholder={placeholder} {...restProps} />;
   }
 
   // The static prefix '../../../../../public/' is a compile-time constant so Turbopack can
@@ -28,6 +28,10 @@ export default async function BlogThumbnail({ src, alt, placeholder, ...restProp
   //
   // The dynamic bit is calculated dynamically from front-matter at server runtime, computed
   // via path.relative for portability.
+  //
+  // Also, due to the limitation of template literal import (whatever bundler), bundler can't static
+  // analyze and tree shake usage, thus can only import everything under the folder.
+  // So we limit the scope to `public/blog/cover` to avoid importing unnecessary files.
   const dynamicBit = path.relative(BLOG_COVER_DIR, path.join(PUBLIC_DIR, src)).replaceAll(path.sep, '/');
   let imgData = await import(`../../../../../public/blog/cover/${dynamicBit}`) as StaticImport;
 
@@ -37,5 +41,5 @@ export default async function BlogThumbnail({ src, alt, placeholder, ...restProp
     imgData = imgData.default;
   }
 
-  return <Image src={imgData} alt={alt} {...restProps} />;
+  return <Image src={imgData} alt={alt} placeholder={placeholder} {...restProps} />;
 }
